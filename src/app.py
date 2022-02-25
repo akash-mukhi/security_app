468bfc34b7f23395a05a096fa5a908fb7b0457ab
Prashant Sharma <sprasha1@netapp.com>
2022-02-25 14:14:40 +0530

    adding changes

--- src/app.py
+++ src/app.py
@@ -1,9 +1,46 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import ssh_connect
+import vulneribility_finder
+from pycvesearch import CVESearch
+
 app = Flask(__name__)
 
-@app.route('/app')
-def hello_world():
-	return 'Hello World'
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        data = "Hackathon"
+        return jsonify({'data': data})
+
+
+@app.route('/GetSecurityPolicyForAllNs', methods=['GET'])
+def get_pod_security_policy():
+    if request.method == 'GET':
+        ssh = ssh_connect.ssh_conncet()
+        data = ssh.run_command_to_sde_machine('kubectl get psp --all-namespaces -o json')
+        return jsonify(data)
+
+
+# @app.route('/name', methods=['GET'])
+# def get_cve_vulnerability():
+#     name = 'mysql'
+#     # vf=vulneribility_finder.cve_vulneribility_finder()
+#     # full_version_cve_list, given_version_cve_list= vf.get_vulnerability(name)
+#     # print(full_version_cve_list, given_version_cve_list)
+#     # return full_version_cve_list, given_version_cve_list
+#     cve = CVESearch('https://cve.circl.lu/')
+#     a=cve.search('mysql')
+#     print(a)
+
+@app.route('/GetAllPod', methods=['GET'])
+def get_all_pods():
+    if request.method == 'GET':
+        ssh = ssh_connect.ssh_conncet()
+        data = ssh.run_command_to_sde_machine('kubectl get pods --all-namespaces -o json')
+        return jsonify(data)
+
+
+
+# driver function
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
---
+++ src/ssh_connect.py
@@ -0,0 +1,25 @@
+import paramiko
+
+
+class ssh_conncet():
+    nbytes = 4096
+    hostname = '10.193.231.237'
+    port = 22
+    input_username = 'root'
+    input_password = 'netapp1!'
+    command = 'ls'
+
+    def run_command_to_sde_machine(self, command):
+        try:
+            client = paramiko.SSHClient()
+            client.load_system_host_keys()
+            client.set_missing_host_key_policy(paramiko.WarningPolicy)
+
+            client.connect(self.hostname, port=22, username=self.input_username, password=self.input_password)
+
+            stdin, stdout, stderr = client.exec_command(command)
+            return stdout.read().decode('UTF-8')
+
+        finally:
+            client.close()
+
